aceb84ffd0613f1f5daaab005fc587feff9fc117
pl3ctrum <64pieces@proton.me>
2025-05-18 11:03:32 +0000

    Add files via upload

---
+++ scripts/toggle-waybar.sh
@@ -0,0 +1,11 @@
+PID_FILE="/tmp/waybar.pid"
+
+if [ -f "$PID_FILE" ]; then
+    # Kill existing Waybar and cleanup
+    kill "$(cat "$PID_FILE")" 2>/dev/null
+    rm -f "$PID_FILE"
+else
+    # Launch Waybar in the background (&) and save its PID correctly
+    waybar -c ~/.config/waybar/waybar.conf &
+    echo $! > "$PID_FILE"
+fi
